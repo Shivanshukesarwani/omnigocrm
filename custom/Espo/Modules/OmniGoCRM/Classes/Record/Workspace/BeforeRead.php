92a8ff40c420353383187aaabee07b9ba173c790
Shivanshu Kesarwani <89177506+Shivanshukesarwani@users.noreply.github.com>
2026-09-26 01:28:21 +0530

    implement EspoCRM ReadHook contract

--- custom/Espo/Modules/OmniGoCRM/Classes/Record/Workspace/BeforeRead.php
+++ custom/Espo/Modules/OmniGoCRM/Classes/Record/Workspace/BeforeRead.php
@@ -3,12 +3,13 @@
 namespace Espo\Modules\OmniGoCRM\Classes\Record\Workspace;
 
 use Espo\Core\Exceptions\Forbidden;
+use Espo\Core\Record\Hook\ReadHook;
 use Espo\Core\Utils\Config;
 use Espo\Entities\User;
 use Espo\ORM\Entity;
 use Espo\Core\Record\ReadParams;
 
-class BeforeRead
+class BeforeRead implements ReadHook
 {
     public function __construct(
         private User $user,
