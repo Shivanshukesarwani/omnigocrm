3a657192132c5d3ea37f8530e84f2972a3006034
Shivanshu Kesarwani <89177506+Shivanshukesarwani@users.noreply.github.com>
2026-09-25 14:11:39 +0530

    feat: update CRM controller

--- backend/app/Http/Controllers/CustomerController.php
+++ backend/app/Http/Controllers/CustomerController.php
@@ -1,8 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use App\Models\Customer;
-class CustomerController extends Controller
-{
-    public function index(){return view('customers.index',['customers'=>Customer::with('contact')->latest()->paginate(20)]);}
-    public function show(Customer $customer){$customer->load(['contact','quotations']);return view('customers.show',['customer'=>$customer]);}
+class CustomerController extends Controller{
+public function index(){return view('customers.index',['customers'=>Customer::with('contact')->latest()->paginate(20)]);}
+public function show(Customer $customer){$customer->load(['contact','quotations','orders','payments']);return view('customers.show',['customer'=>$customer]);}
 }
