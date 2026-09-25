b6766b3088098f1bfc47f67f6f2d430e519d2be9
Shivanshu Kesarwani <89177506+Shivanshukesarwani@users.noreply.github.com>
2026-09-25 14:04:32 +0530

    feat: add CRM model MessageTemplate.php

---
+++ backend/app/Models/MessageTemplate.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class MessageTemplate extends Model {
+ protected $fillable=['name','situation','body','active'];
+ protected $casts=['active'=>'boolean'];
+}
