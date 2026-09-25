f45b1a1a86437e4a9063e984262034a12471067e
Shivanshu Kesarwani <89177506+Shivanshukesarwani@users.noreply.github.com>
2026-09-26 01:34:00 +0530

    fix Meta webhook response construction

--- custom/Espo/Modules/OmniGoCRM/Api/GetMetaLeadWebhook.php
+++ custom/Espo/Modules/OmniGoCRM/Api/GetMetaLeadWebhook.php
@@ -1,16 +1,29 @@
 <?php
+
 namespace Espo\Modules\OmniGoCRM\Api;
+
 use Espo\Core\Api\Action;
 use Espo\Core\Api\Request;
 use Espo\Core\Api\Response;
+use Espo\Core\Api\ResponseComposer;
 use Espo\Modules\OmniGoCRM\Services\LeadSourceWebhookService;
-class GetMetaLeadWebhook implements Action {
-    public function __construct(private LeadSourceWebhookService $service) {}
-    public function process(Request $request): Response {
-        return new \Espo\Core\Api\Response($this->service->verifyMeta(
-            (string) ($request->getQueryParams()['hub_mode'] ?? ''),
-            (string) ($request->getQueryParams()['hub_verify_token'] ?? ''),
-            (string) ($request->getQueryParams()['hub_challenge'] ?? '')
-        ));
+
+class GetMetaLeadWebhook implements Action
+{
+    public function __construct(
+        private LeadSourceWebhookService $service,
+    ) {}
+
+    public function process(Request $request): Response
+    {
+        $params = $request->getQueryParams();
+
+        return ResponseComposer::empty()->writeBody(
+            $this->service->verifyMeta(
+                (string) ($params['hub_mode'] ?? ''),
+                (string) ($params['hub_verify_token'] ?? ''),
+                (string) ($params['hub_challenge'] ?? ''),
+            )
+        );
     }
 }
