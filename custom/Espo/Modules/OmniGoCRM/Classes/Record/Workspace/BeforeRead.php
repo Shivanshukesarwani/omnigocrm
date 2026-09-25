66876cf3ddc75deb8aa761beac4867e20c9440f2
Shivanshu Kesarwani <89177506+Shivanshukesarwani@users.noreply.github.com>
2026-09-25 20:23:01 +0530

    Permit trusted Espo system job context

--- custom/Espo/Modules/OmniGoCRM/Classes/Record/Workspace/BeforeRead.php
+++ custom/Espo/Modules/OmniGoCRM/Classes/Record/Workspace/BeforeRead.php
@@ -21,6 +21,10 @@ class BeforeRead
             return;
         }
 
+        if ($this->user->isSystem()) {
+            return;
+        }
+
         if ((bool) $this->config->get('omniGoCRMSaaSAdminBypass') && $this->user->isAdmin()) {
             return;
         }
