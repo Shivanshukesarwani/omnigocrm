ab64089623c8358db62e88f7fa36ad987473b2be
Shivanshu Kesarwani <89177506+Shivanshukesarwani@users.noreply.github.com>
2026-09-25 20:07:07 +0530

    Use configurable iOS default CRM URL

--- ios/OmniGoCRM/AppConfig.swift
+++ ios/OmniGoCRM/AppConfig.swift
@@ -1,6 +1,5 @@
 import Foundation
 
 enum AppConfig {
-    static let baseURL = URL(string: "https://crm.example.com/")!
-    static let apiURL = baseURL.appendingPathComponent("api/v1/")
+    static let defaultBaseURL = URL(string: "https://crm.example.com/")!
 }
